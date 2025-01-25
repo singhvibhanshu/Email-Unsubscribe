ecf7388c9752317b78ccc713b284ce19bf0d785c
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-01-25 13:16:10 +0530

    updated: finding links

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import imaplib
 import email
 import os
+from bs4 import BeautifulSoup
 
 load_dotenv()
 
@@ -14,10 +15,17 @@ def connect_to_mail():
     mail.select("inbox")
     return mail
 
+def extract_links_from_html(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    links = [link["href"] for link in soup.find_all("a", href=True) if "unsubscribe" in link["href"].lower()]
+    return links
+
 def search_for_email():
     mail = connect_to_mail()
     _, search_data = mail.search(None, 'TEXT "unsubscribe"')
     data = search_data[0].split()
+
+    links = []
 
     for num in data:
         _, data = mail.fetch(num, "(RFC822)")
@@ -27,14 +35,16 @@ def search_for_email():
             for part in msg.walk():
                 if part.get_content_type() == "text/html":
                     html_content = part.get_payload(decode=True).decode(errors="replace")
-                    print(html_content)
+                    links.extend(extract_links_from_html(html_content))
         else:
             content_type = msg.get_content_type()
             content = msg.get_payload(decode=True).decode(errors="replace")
 
             if content_type == "text/html":
-                print(content)
+                links.extend(extract_links_from_html(content))
 
     mail.logout()
+    return links
 
-search_for_email()
+links = search_for_email()
+print(links)
