2657e5b801f5a01c9d334921d6a221b3fb2f4f5e
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-01-25 11:24:00 +0530

    reading emails fn added

--- main.py
+++ main.py
@@ -12,3 +12,28 @@ def connect_to_mail():
     mail.login(username, password)
     mail.select("inbox")
     return mail
+
+def search_for_mail():
+    mail = connect_to_mail()
+    _, search_data = mail.search(None, '(BODY, "unsubscribe")')
+    data = search_data[0].split()
+
+    for num in data:
+        _, data = mail.fetch(num, "(RFC822)")
+        msg = email.message_from_bytes(data[0][1])
+
+        if msg.is_multipart():
+            for part in msg.walk():
+                if part.get_content_type() == "text/html":
+                    html_content = part.get_payload(decode=True).decode()
+                    print(html_content)
+        else:
+            content_type = msg.get_content_type()
+            content = msg.get_payload(decode=True).decode()
+
+            if content_type == "tect/html":
+                print(content)
+
+    mail.logout()
+
+search_for_mail()
