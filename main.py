642f89ccde6e129b26c0ba97d4b9d04ce791799f
Vibhanshu Singh <135852806+singhvibhanshu@users.noreply.github.com>
2025-01-25 10:00:18 +0530

    connecting to gmail

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from dotenv import load_dotenv
+import imaplib
+import email
 import os
 load_dotenv()
 
 username = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
+
+def connect_to_mail():
+    mail = imaplib.IMAP4_SSL("imap.gmail.com")
+    mail.login(username, password)
+    mail.select("inbox")
+    return mail
